1b9cc2ba55621b49456ad48f86dfccbed20e0c58
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-09-13 11:55:56 -0300

    .

--- src/main/java/adapters/analyzer/LinterAdapter.java
+++ src/main/java/adapters/analyzer/LinterAdapter.java
@@ -1,13 +1,48 @@
 package adapters.analyzer;
 
+import adapters.formatter.FormatterErrorHandler;
+import adapters.version.VersionAdapter;
 import interpreter.ErrorHandler;
 import interpreter.PrintScriptLinter;
+import org.gudelker.DefaultFormatter;
+import org.gudelker.DefaultFormatterFactory;
+import org.gudelker.rules.FormatterRule;
+import org.gudelker.rules.InputStreamFormatterConfigLoaderToMap;
+import org.gudelker.sourcereader.InputStreamSourceReader;
+import org.gudelker.statements.interfaces.Statement;
+import org.gudelker.tokenstream.TokenStream;
+import org.gudelker.utilities.Version;
+import runners.lexer.LexerRunner;
+import runners.lexer.LexerRunnerResult;
+import runners.parser.ParserRunner;
+import runners.parser.ParserRunnerResult;
 
 import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 public class LinterAdapter implements PrintScriptLinter {
     @Override
     public void lint(InputStream src, String version, InputStream config, ErrorHandler handler) {
+        Version v = VersionAdapter.toVersion(version);
+        // lexer
+        LexerRunner lexer = new LexerRunner();
+        InputStreamSourceReader inputStreamSourceReader = new InputStreamSourceReader(src,8192);
+        FormatterErrorHandler errorHandler = new FormatterErrorHandler();
+        LexerRunnerResult tokensResult = lexer.runLexer(inputStreamSourceReader, v, errorHandler);
+
+        // parser
+        TokenStream tokenStream = new TokenStream(tokensResult.getTokens());
+        ParserRunner parserRunner = new ParserRunner();
+        ParserRunnerResult parserRunnerResult = parserRunner.runParser(tokenStream, v, errorHandler);
+
+        // format
+        DefaultFormatter formatter = DefaultFormatterFactory.INSTANCE.createFormatter(v);
+        InputStreamFormatterConfigLoaderToMap loader = new InputStreamFormatterConfigLoaderToMap(config);
+        Map<String, FormatterRule> rules = loader.loadConfig();
+
+        List<Statement> statements = parserRunnerResult.getStatements();
+
 
     }
 }
