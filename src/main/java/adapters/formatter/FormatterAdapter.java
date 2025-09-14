2922518ec83a82bce38059ad81632ea6a4d8e886
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-09-14 02:43:33 -0300

    .

--- src/main/java/adapters/formatter/FormatterAdapter.java
+++ src/main/java/adapters/formatter/FormatterAdapter.java
@@ -4,11 +4,11 @@ import adapters.version.VersionAdapter;
 import interpreter.PrintScriptFormatter;
 import org.gudelker.DefaultFormatter;
 import org.gudelker.DefaultFormatterFactory;
+import org.gudelker.parser.tokenstream.TokenStream;
 import org.gudelker.rules.FormatterRule;
 import org.gudelker.rules.InputStreamFormatterConfigLoaderToMap;
 import org.gudelker.sourcereader.InputStreamSourceReader;
 import org.gudelker.statements.interfaces.Statement;
-import org.gudelker.parser.parser.tokenstream.TokenStream;
 import org.gudelker.utilities.Version;
 import runners.lexer.LexerRunner;
 import runners.lexer.LexerRunnerResult;
@@ -32,33 +32,16 @@ public class FormatterAdapter implements PrintScriptFormatter {
         FormatterErrorHandler errorHandler = new FormatterErrorHandler();
         LexerRunnerResult tokensResult = lexer.runLexer(inputStreamSourceReader, v, errorHandler);
 
-        // parser
-        TokenStream tokenStream = new TokenStream(tokensResult.getTokens());
-        ParserRunner parserRunner = new ParserRunner();
-        ParserRunnerResult parserRunnerResult = parserRunner.runParser(tokenStream, v, errorHandler);
-
         // format
-        DefaultFormatter formatter = DefaultFormatterFactory.INSTANCE.createFormatter(v);
         InputStreamFormatterConfigLoaderToMap loader = new InputStreamFormatterConfigLoaderToMap(config);
         Map<String, FormatterRule> rules = loader.loadConfig();
-
-        List<Statement> statements = parserRunnerResult.getStatements();
-        int lastIndex = statements.size() - 1;
-
-        for (int i = 0; i < statements.size(); i++) {
-            Statement statement = statements.get(i);
-            String formatted = formatter.format(statement, rules);
-
-            if (i == lastIndex && formatted.endsWith("\n")) {
-                formatted = formatted.substring(0, formatted.length() - 1);
-            }
-
-            try {
-                writer.write(formatted);
-            } catch (Exception e) {
-                //
-            }
+        TokenStream tokenStream = new TokenStream(tokensResult.getTokens());
+        DefaultFormatter formatter = new DefaultFormatter();
+        String formatted = formatter.format(tokenStream, rules);
+        try {
+            writer.write(formatted);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-
     }
 }
